99569b6033017b0d6808110d0f245393ede3720f
Luis Henrique Ramos Soares <89323852+luishh205@users.noreply.github.com>
2024-11-13 08:33:03 -0300

    Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+//css
 import './App.css';
+//react
+import { useCallback,useEffect,useState } from 'react';
+//data
+import { wordsList } from './Data/words.js';
+//components
+import StartScreen from './Components/StartScreen';
+import Game from './Components/Game.js';
+import GameOver from './Components/GameOver.js';
+
+const stages = [
+  { id:1, name: "start" },
+  { id:2, name: "game"  },
+  { id:3, name: "end" } 
+];
 
 function App() {
+const [gameStage, setGameStage] = useState(stages[0].name);
+const [words] = useState(wordsList);
+
+const [pickedWord, setpickedWord] = useState("");
+const [pickedCategory, setpickedCategory] = useState("");
+const [letters, setletters] = useState([]);
+
+const [guessedLetters, setGuessedLetters] = useState([]);
+const [wrongLetters, setWrongLetters] = useState([]);
+const [guesses, setGuesses] = useState(3);
+
+const pickWordAndCategory = ()=>{
+  const categories = Object.keys(words);
+  const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+  const word = words[category][Math.floor(Math.random() * words[category].length)];
+  
+  //console.log(category);
+  //console.log(word);
+
+  return {word, category};
+}
+
+const startGame = () => {
+
+  const { word, category } = pickWordAndCategory();
+
+  let worLetters = word.split("");
+
+  worLetters = worLetters.map((l) => l.toLowerCase());
+
+  setpickedWord(word);
+  setpickedCategory(category);
+  setletters(worLetters);
+
+  console.log(worLetters);
+  setGameStage(stages[1].name);
+}
+
+const verifyLetter = () => {
+  setGameStage(stages[2].name)
+}
+
+const retry = () => {
+  setGameStage(stages[0].name)
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {gameStage === "start" && <StartScreen startGame={startGame} />}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameStage === "end" && <GameOver retry={retry}/>}
     </div>
   );
 }
